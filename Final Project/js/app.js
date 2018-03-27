db8f463db33f62d48962eeefedeefa9318433df4
angelinejefferson <35539099+angelinejefferson@users.noreply.github.com>
2018-03-27 19:56:59 -0400

    First upload of Final Project

---
+++ Final Project/js/app.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function(){
+	window.sr = ScrollReveal({ reset: true });
+	sr.reveal('.timeSOS', { reset: true, duration: 500, delay: 300, scale: 1.5, mobile: true});
+	sr.reveal('.aboutSOS', { reset: true, duration: 500, delay: 300, scale: 1.5, mobile: true});
+	sr.reveal('.whoHonorees', { reset: true, duration: 500, delay: 300, scale: 1.5, mobile: true});
+	sr.reveal('.theHonorees', { reset: true, duration: 500, delay: 300, scale: 1.5, mobile: true});
+	sr.reveal('.thursdayNetwork', { reset: true, duration: 500, delay: 300, scale: 1.5, mobile: true});
+	sr.reveal('.contactUs', { reset: true, duration: 500, delay: 300, scale: 1.5, mobile: true});
+
+})
