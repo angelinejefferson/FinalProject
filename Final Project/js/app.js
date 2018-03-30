2b48eeda6dffeb04ab0abfb0157ea11cc5904164
angelinejefferson <35539099+angelinejefferson@users.noreply.github.com>
2018-03-30 12:42:29 -0400

    Add files via upload

--- Final Project/js/app.js
+++ Final Project/js/app.js
@@ -1,10 +1,69 @@
+
 jQuery(document).ready(function(){
 	window.sr = ScrollReveal({ reset: true });
-	sr.reveal('.timeSOS', { reset: true, duration: 500, delay: 300, scale: 1.5, mobile: true});
-	sr.reveal('.aboutSOS', { reset: true, duration: 500, delay: 300, scale: 1.5, mobile: true});
-	sr.reveal('.whoHonorees', { reset: true, duration: 500, delay: 300, scale: 1.5, mobile: true});
-	sr.reveal('.theHonorees', { reset: true, duration: 500, delay: 300, scale: 1.5, mobile: true});
-	sr.reveal('.thursdayNetwork', { reset: true, duration: 500, delay: 300, scale: 1.5, mobile: true});
-	sr.reveal('.contactUs', { reset: true, duration: 500, delay: 300, scale: 1.5, mobile: true});
 
+	//sr.reveal('.backgroundImg', { 
+		//reset: true, 
+		//duration: 500, 
+		//delay: 300, 
+		//scale: 1.5, 
+		//mobile: true});
+	sr.reveal('.aboutSOS', { 
+		reset: true, 
+		duration: 500,
+		delay: 300, 
+		scale: 1.5,
+		easing: 'ease-in-out', 
+		mobile: true});
+	sr.reveal('.whoHonorees', { 
+		reset: true, 
+		duration: 500, 
+		delay: 300, 
+		easing: 'ease-in-out',
+		scale: 0.9, 
+		mobile: true});
+	sr.reveal('.listHonorees', { 
+		reset: true, 
+		duration: 500, 
+		delay: 300,
+		viewFactor: 0.3,
+		easing: 'ease-in-out', 
+		scale: 1.5, 
+		mobile: true});
+	sr.reveal('.thursdayNetwork', { 
+		reset: true, 
+		duration: 500, 
+		delay: 300, 
+		scale: 1.5, 
+		easing: 'ease-in-out',		
+		mobile: true});
+	sr.reveal('.answerTN', { 
+		reset: true, 
+		duration: 500, 
+		delay: 300, 
+		easing: 'ease-in-out',
+		scale: 1.5, 
+		mobile: true});
+	//sr.reveal('.contactUs', { 
+	//	reset: true, 
+	//	duration: 500, 
+	//	delay: 300, 		
+	//	scale: 1.5, 
+	//	mobile: true} );
 })
+
+jQuery(window).on("scroll touchmove", function(){
+
+	if ($(document).scrollTop() >= $(".whoHonorees").position().top){
+	 jQuery('body').css('background-color','#cbdfeb');
+	}
+
+});
+
+
+
+	
+
+jQuery('.contactUs').append();
+
+//afterReveal: function('.contactUs'){jQuery('.typewriter h2').addClass('type')}
